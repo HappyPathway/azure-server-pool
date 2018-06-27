7fdc969c609b8419d3a26d4139064c91457f8f98
Dave Arnold <darnold@hashicorp.com>
2018-06-26 18:20:22 -0700

    splitting network and server pool

--- main.tf
+++ main.tf
@@ -7,25 +7,44 @@ variable "server_pool_count" {
 
 variable "service_name" {}
 
-variable "env" {}
+variable "system_user" {}
+variable "system_password" {}
+
+variable "organization" {}
+
+variable "network_ws" {}
 
 //--------------------------------------------------------------------
 // Modules
-module "network" {
-  source       = "app.terraform.io/Darnold-Hashicorp/network/azurerm"
-  version      = "1.0.11"
-  env          = "${var.env}"
-  service_name = "${var.service_name}"
+
+variable "resource_tags" {
+  type = "map"
+
+  default = {
+    Owner       = "darnold"
+    TTL         = 48
+    ClusterName = "consul-demos"
+  }
+}
+
+//--------------------------------------------------------------------
+data "terraform_remote_state" "network" {
+  backend = "atlas"
+
+  config {
+    name = "${var.organization}/${var.network_ws}"
+  }
 }
 
 module "server_pool" {
   source  = "app.terraform.io/Darnold-Hashicorp/server-pool/azurerm"
-  version = "1.0.6"
+  version = "1.0.8"
 
-  count          = "${var.server_pool_count}"
-  resource_group = "${var.servcie_name}"
-  subnet         = "${var.service_name}-subnet1"
-  env            = "${var.env}"
-  service_name   = "${var.service_name}"
-  location       = "${module.network.location}"
+  count           = "${var.server_pool_count}"
+  resource_group  = "${data.terraform_remote_state.network.rg_name}"
+  subnet          = "${data.terraform_remote_state.network.virtual_network_name}-subnet1"
+  service_name    = "${var.service_name}"
+  location        = "${data.terraform_remote_state.network.location}"
+  system_user     = "${var.system_user}"
+  system_password = "${var.system_password}"
 }
