d2bbfb36208e53d850bd28bb6f4d7012230f588d
Dave Arnold <darnold@hashicorp.com>
2018-07-06 08:44:17 -0700

    fixing credentials path in vault

--- vault.tf
+++ vault.tf
@@ -8,6 +8,6 @@ data "template_file" "credentials" {
 }
 
 resource "vault_generic_secret" "credentials" {
-  path      = "secret/${var.service_name}/${var.service_version}/${var.env}/credentials"
+  path      = "secret/serverpool/${var.service_name}/${var.service_version}/${var.env}/credentials"
   data_json = "${template_file.credentials.rendered}"
 }
