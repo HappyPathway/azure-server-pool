3e3996afd20c91dee5fd4b882fa0338b22b68b2a
Dave Arnold <darnold@hashicorp.com>
2018-07-06 08:46:08 -0700

    fixing template render

--- vault.tf
+++ vault.tf
@@ -9,5 +9,5 @@ data "template_file" "credentials" {
 
 resource "vault_generic_secret" "credentials" {
   path      = "secret/serverpool/${var.service_name}/${var.service_version}/${var.env}/credentials"
-  data_json = "${template_file.credentials.rendered}"
+  data_json = "${data.template_file.credentials.rendered}"
 }
