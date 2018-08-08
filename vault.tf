f8880f7117fc02b9ccd61996d9418f89606931af
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-08 12:32:44 -0700

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -8,6 +8,6 @@ data "template_file" "credentials" {
 }
 
 resource "vault_generic_secret" "credentials" {
-  path      = "secret/serverpool/${var.service_name}/${var.service_version}/${var.env}/credentials"
+  path      = "secret/${var.service_name}/${var.service_version}/${var.env}/credentials"
   data_json = "${data.template_file.credentials.rendered}"
 }
