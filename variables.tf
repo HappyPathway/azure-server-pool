2a199be2f05487ca37dbe3be7d00d9c9088bf47f
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-22 10:25:41 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,16 +10,6 @@ variable "organization" {}
 
 variable "network_ws" {}
 
-variable "vault_addr" {
-  type        = "string"
-  description = "Address of Vault Cluster"
-}
-
-variable "vault_token" {
-  type        = "string"
-  description = "Vault Token"
-}
-
 variable "env" {}
 
 variable "default_lease_ttl" {
@@ -28,14 +18,4 @@ variable "default_lease_ttl" {
 
 variable "max_lease_ttl" {
   default = 600
-}
-
-variable "app_role_secret_id" {
-  type        = "string"
-  description = "App Role Secret ID"
-}
-
-variable "db_server_name" {
-  type = "string"
-  description = "Database Server Name"
 }
