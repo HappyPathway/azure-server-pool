67d64f69bad773bb4c079d4fb0579620c79d9977
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-22 10:44:13 -0700

    Delete db_connect.tf

--- db_connect.tf
+++
@@ -1,9 +0,0 @@
-module "db-connect" {
-  source  = "app.terraform.io/Darnold-AzureTF/db-connect/azure"
-  version = "1.0.0"
-  access_name = "${var.service_name}"
-  db_server_name = "${var.db_server_name}"
-  ip_address = "${module.server_pool.ipaddress}"
-  resource_group = "${data.terraform_remote_state.network.rg_name}"
-
-}
