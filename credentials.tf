4151d4f8b5fb43ddddadff38ec6ad68c41c90dd2
Dave Arnold <dja+djaboxx@sonic.net>
2018-09-04 13:13:09 -0700

    Update credentials.tf

--- credentials.tf
+++ credentials.tf
@@ -1,9 +1,17 @@
 resource "random_string" "username" {
   length  = 24
   special = false
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ami_id = "${module.server_pool.ipaddress}"
+  }
 }
 
 resource "random_string" "password" {
   length  = 24
   special = true
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ami_id = "${module.server_pool.ipaddress}"
+  }
 }
